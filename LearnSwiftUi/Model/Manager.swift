1de75685a6730eefa373e1e497f9b59fbe8a0372
kepsonDiaz <kherabadiaby328@gmail.com>
2023-05-26 12:16:29 +0000

    add the team model and other assets

---
+++ LearnSwiftUi/Model/Manager.swift
@@ -0,0 +1,8 @@
+//
+//  Manager.swift
+//  LearnSwiftUi
+//
+//  Created by Kheraba on 26/05/2023.
+//
+
+import Foundation
---
+++ LearnSwiftUi/Model/Team.swift
@@ -0,0 +1,8 @@
+//
+//  Team.swift
+//  LearnSwiftUi
+//
+//  Created by Kheraba on 26/05/2023.
+//
+
+import Foundation
