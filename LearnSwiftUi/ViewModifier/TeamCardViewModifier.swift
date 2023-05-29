a1baa6e3f3db9d94c5510fd0ac560236a125454e
kepsonDiaz <kherabadiaby328@gmail.com>
2023-05-29 10:00:05 +0000

    add view modifier

--- LearnSwiftUi/ViewModifier/TeamCardViewModifier.swift
+++ LearnSwiftUi/ViewModifier/TeamCardViewModifier.swift
@@ -5,4 +5,25 @@
 //  Created by Kheraba on 26/05/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct TeamCardViewModifier: ViewModifier {
+    
+    let teamType: TeamType
+    
+    func body(content: Content) -> some View {
+        content
+            .padding(8)
+            .foregroundColor(.white)
+            .background(teamType.background)
+            .cornerRadius(10)
+    }
+}
+
+extension View {
+    func applyTeamCardViewStyle(teamType: TeamType) -> some View{
+        self.modifier(TeamCardViewModifier(teamType: teamType))
+    }
+}
+
+
