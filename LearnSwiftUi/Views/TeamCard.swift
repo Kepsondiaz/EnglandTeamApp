602bbbfcf5bc1620e66d39ef59228331f4dcb46a
kepsonDiaz <kherabadiaby328@gmail.com>
2023-05-29 10:01:12 +0000

    add new TeamCard

--- LearnSwiftUi/Views/TeamCard.swift
+++ LearnSwiftUi/Views/TeamCard.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct TeamCard: View {
+    
+    let team: Team
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            HStack(alignment: .top, spacing: 12) {
+                
+                Image(team.id.badge)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:50, height:50)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    
+                    Text(team.name)
+                        .font(.system(size: 18, weight: .bold))
+                    Text("Founded: \(team.founded)")
+                        .font(.system(size: 12, weight: .light))
+                    Text("Current \(team.manager.job.rawValue): \(team.manager.name)")
+                        .font(.system(size: 10, weight: .light))
+                    Text(team.info)
+                        .font(.system(size: 12, weight: .medium))
+                }
+                
+                Spacer()
+                
+                Button(action: {}, label: {
+                    Image(systemName: "play.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                })
+                .frame(maxWidth: 40, maxHeight: .infinity, alignment: .center)
+            }
+            
+        }
+        .applyTeamCardViewStyle(teamType: team.id)
     }
 }
 
 struct TeamCard_Previews: PreviewProvider {
     static var previews: some View {
-        TeamCard()
+        Group {
+            TeamCard(team: Team.dummyData[0])
+                .previewLayout(PreviewLayout.fixed(width: 400, height: 150))
+                .padding()
+            .previewDisplayName("Dummy Data 1: Team Card preview")
+            
+            TeamCard(team: Team.dummyData[1])
+                .previewLayout(PreviewLayout.fixed(width: 400, height: 150))
+                .padding()
+            .previewDisplayName("Dummy Data 2: Team Card preview")
+        }
     }
 }
